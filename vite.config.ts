8f8ef3978b7e63704ae105d025eb403a9730dc83
shibam0005 <shibam681@gmail.com>
2026-01-15 18:08:09 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Arun-s-Cafe-final/',
   server: {
     port: 5173,
     strictPort: true
